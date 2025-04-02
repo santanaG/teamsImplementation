55a69eef37a26a4dfd1990a5aeab641b861a16dd
santanaG <gs8777@gmail.com>
2025-04-02 06:57:17 -0400

    fix graph data assumption

--- messages/requests/graph.js
+++ messages/requests/graph.js
@@ -2,13 +2,15 @@ import axios from 'axios'
 import formatAxiosError from '../utilities/formatAxiosError.js'
 
 export default (aadObjectId, Authorization) => axios.get(
-  `https://graph.microsoft.com/v1.0/users/${aadObjectId}`,
+  `https://graph.microsoft.com/v1.0/users/${
+    aadObjectId
+  }?$select=displayName,mail,department,jobTitle`,
   { headers: { Authorization } }
 )
   .catch(error => Promise.reject(formatAxiosError(error, 'Graph API request')))
   .then(({ data }) => ({
     fullName: data.displayName,
-    email: data.mail || data.userPrincipalName,
+    email: data.mail,
     title: data.jobTitle,
     department: data.department
   }))
